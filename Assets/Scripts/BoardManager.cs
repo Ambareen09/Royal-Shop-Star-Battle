85bb000b030a75ff6152d1412b36f95be54a4b58
Ambareen09 <ambubushra@gmail.com>
2021-06-04 21:19:07 +0530

    Prefabs created, Raycasting in all direction done

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        
+        RayCastDetectUp();
+        RayCastDetectDown();
+        RayCastDetectLeft();
+        RayCastDetectRight();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RayCastDetectUp()
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up,20f);
+
+        if (hit)
+        {
+            Debug.Log("We hit " + hit.collider.name);
+            Debug.DrawRay(transform.position, Vector2.up, Color.red, 25f);
+        }
     }
+
+    private void RayCastDetectDown()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,20f);
+
+        if (hit)
+        {
+            Debug.Log("We hit " + hit.collider.name);
+            Debug.DrawRay(transform.position, Vector2.down, Color.blue, 25f);
+        }
+    }
+
+    private void RayCastDetectLeft()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left,20f);
+
+        if (hit)
+        {
+            Debug.Log("We hit " + hit.collider.name);
+            Debug.DrawRay(transform.position, Vector2.left, Color.black, 25f);
+        }
+    }
+
+    private void RayCastDetectRight()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right,20f);
+
+        if (hit)
+        {
+            Debug.Log("We hit " + hit.collider.name);
+            Debug.DrawRay(transform.position, Vector2.right, Color.magenta, 25f);
+        }
+    }
+    
+    
 }
