3d0ef0e28965470c2d10c6f6307f22aae527374b
Ambareen09 <ambubushra@gmail.com>
2021-07-02 17:15:51 +0530

    UI - 2

---
+++ Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelHandler : MonoBehaviour
+{
+    public Button[] levelButtons;
+
+    public void Start()
+    {
+        var levelAt = PlayerPrefs.GetInt("levelAt", 1);
+
+        for (var i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelAt)
+                levelButtons[i].interactable = false;
+        }
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
