c94ef992f8f881b04ba7985b3d794400c83b7776
Ambareen09 <ambubushra@gmail.com>
2021-07-02 23:55:37 +0530

    Level lock/unlock function added in UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int nextScene;
+    public void Start()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    public void WintoNextLevel()
+    {
+        
+        SceneManager.LoadScene(nextScene);
+        if (nextScene > PlayerPrefs.GetInt("levelAt"))
+        {
+            PlayerPrefs.SetInt("levelAt", nextScene);
+        }
+    }
     
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
--- Assets/Scripts/LevelHandler.cs
+++ Assets/Scripts/LevelHandler.cs
@@ -1,19 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelHandler : MonoBehaviour
 {
+    public Image locked;
+    public Text unlocked;
     public Button[] levelButtons;
+    private int _levelAt;
 
     public void Start()
     {
-        var levelAt = PlayerPrefs.GetInt("levelAt", 1);
+        _levelAt = PlayerPrefs.GetInt("levelAt", 1);
+        UnlockLevel();
+    }
 
+    private void UnlockLevel()
+    {
         for (var i = 0; i < levelButtons.Length; i++)
         {
-            if (i + 1 > levelAt)
+            if (i + 1 > _levelAt)
+            {
                 levelButtons[i].interactable = false;
+            }
+            if (levelButtons[i].interactable)
+            {
+                gameObject.transform.GetChild(0).GetChild(i).GetChild(0).gameObject.SetActive(true);
+                gameObject.transform.GetChild(0).GetChild(i).GetChild(1).gameObject.SetActive(false);
+            }
         }
+        
+    }
+    
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
     }
 }
